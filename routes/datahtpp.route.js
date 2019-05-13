4636c3305684013fab577551d5896f26fb51d44b
Guillaume <guillaumecassoly@hotmail.fr>
2019-05-13 19:32:09 +0200

    finition des routes pour competitions

--- routes/datahtpp.route.js
+++ routes/datahtpp.route.js
@@ -15,7 +15,7 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-
+/////////////////TEAMS///////////////
 router.get('/teams/:id',async (req,res)=>{
   request({
     json : true,
@@ -46,6 +46,7 @@ router.get('/teams',async(req,res)=>{
     });
   });
 })
+/////////////////MATCHES///////////////
 router.get('/matches',async (req,res)=>{
   var before = false;
   MongoClient.connect(url, {useNewUrlParser: true},function(err, db) {
@@ -66,7 +67,56 @@ router.get('/matches',async (req,res)=>{
   });
     
 })
-router.get('/competitions',async(req,res)=>{
+/////////////////COMPETITIONS///////////////
+router.get('/competitions/:id/matches',async(req,res)=>{    //matchs par competition
+  request({
+    json : true,
+    uri: "https://api.football-data.org/v2/competitions/" + req.params.id + "/matches",
+    headers : {
+      'X-Auth-Token' : config.token
+    }
+  }, (err, result, body) => {
+  if (err) { return console.log(err); }
+  res.json(body.matches) ;
+  });
+})
+router.get('/competitions/:id/standings',async(req,res)=>{      //classement par comeptition
+  request({
+    json : true,
+    uri: "https://api.football-data.org/v2/competitions/" + req.params.id + "/standings",
+    headers : {
+      'X-Auth-Token' : config.token
+    }
+  }, (err, result, body) => {
+  if (err) { return console.log(err); }
+  res.json(body.standings) ;
+  });
+})
+router.get('/competitions/:id/scorers',async(req,res)=>{      //buts par competitions
+  request({
+    json : true,
+    uri: "https://api.football-data.org/v2/competitions/" + req.params.id + "/scorers",
+    headers : {
+      'X-Auth-Token' : config.token
+    }
+  }, (err, result, body) => {
+  if (err) { return console.log(err); }
+  res.json(body.scorers) ;
+  });
+})
+router.get('/competitions/:id/teams',async(req,res)=>{    //team par competitions
+  request({
+    json : true,
+    uri: "https://api.football-data.org/v2/competitions/" + req.params.id + "/teams",
+    headers : {
+      'X-Auth-Token' : config.token
+    }
+  }, (err, result, body) => {
+  if (err) { return console.log(err); }
+  res.json(body.teams) ;
+  });
+})
+router.get('/competitions',async(req,res)=>{            //competitions
   var before = false;
   MongoClient.connect(url, {useNewUrlParser: true},function(err, db) {
     if (err) throw err;
