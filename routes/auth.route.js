2e7eeec4e0b76b926063d7b70c1f7a69994874b6
Myst-Click <guillaumecassoly@hotmail.fr>
2019-06-26 13:31:58 +0200

    user information

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -62,7 +62,7 @@ router.post('/login',async(req,res)=>{
 
                 res.json({
                     success: true,
-                    user : user,
+                    user: user,
                     message: 'Authentification reussie',
                     token: token
                   });
